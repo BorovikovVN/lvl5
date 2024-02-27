67e09fc22a46e18791c8f14c2d2bca87f6cbf1b4
Borovikov <vadim.102055@gmail.com>
2024-02-27 19:43:16 +0400

    Initial commit

---
+++ lvl5/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lvl5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            listv.Items.Add(new ListItem());
+            listv.Items.Add(new ListItem());
+            listv.Items.Add(new ListItem());
+            listv.Items.Add(new ListItem());
+            listv.Items.Add(new ListItem());
+            ComboBox1.Items.Add("Элемент1");
+            ComboBox1.Items.Add("Элемент2");
+            ComboBox1.Items.Add("Элемент3");
+
+            this.Title = "Уровень 5";
+        }
+    }
+}
---
+++ lvl5/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lvl5.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            MaterialHasProducts = new HashSet<MaterialHasProduct>();
+        }
+
+        public int IdProduct { get; set; }
+        public string NameProduct { get; set; } = null!;
+        public int TypeProduct { get; set; }
+        public int Price { get; set; }
+        public string Article { get; set; } = null!;
+        public byte[]? Photo { get; set; }
+
+        public virtual ProductType TypeProductNavigation { get; set; } = null!;
+        public virtual ICollection<MaterialHasProduct> MaterialHasProducts { get; set; }
+    }
+}
